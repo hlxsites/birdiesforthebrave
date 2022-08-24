76060d150989a8158bcf4b02a89559088c769f95
Lars Trieloff <lars@trieloff.net>
2022-08-24 16:01:22 +0200

    fix(carousel): steal h1 and image back from hero block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -41,68 +41,15 @@ export default async function decorate(block) {
     classes.forEach((e, j) => {
       if (row.children[j]) row.children[j].classList.add(`carousel-${e}`);
     });
-    /* course carousel */
-    if (blockClasses.includes('course')) {
+    if (i === 0) {
+      const image = row.querySelector('.carousel-image');
       const text = row.querySelector('.carousel-text');
-      text.classList.add('course-text');
-      // setup overview (title, img, desc)
-      const overview = document.createElement('div');
-      overview.classList.add('course-overview');
-      overview.append(
-        text.querySelector('h2'), // title
-        text.querySelector('h2 + h3'), // par heading
-        text.querySelector('h2 + h3 + p'), // course img
-        text.querySelector('h2 + h3 + p + p'), // course desc
-      );
-      const holeImg = overview.querySelector('picture');
-      if (holeImg) holeImg.parentNode.classList.add('course-hole');
-      // setup stats
-      const statistics = document.createElement('div');
-      statistics.classList.add('course-statistics');
-      statistics.append(
-        text.querySelector('h3'), // statistics heading
-        text.querySelector('h3 + ul'), // statistics list
-      );
-      const statsTable = document.createElement('table');
-      const allStats = statistics.querySelector('ul');
-      let stats = [];
-      if (allStats) stats = allStats.querySelectorAll('li');
-      stats.forEach((s) => {
-        const stat = s.querySelector('strong').textContent;
-        // setup scoring average ring
-        if (stat.toUpperCase() === 'SCORING AVG') {
-          const avg = document.createElement('div');
-          avg.classList.add('course-avg');
-          avg.innerHTML = `<p>${s.innerHTML}</p>`;
-          allStats.parentNode.insertBefore(avg, allStats);
-          s.remove();
-        } else {
-          const tableRow = document.createElement('tr');
-          tableRow.classList.add(`course-${toClassName(stat)}`);
-          const val = parseInt(s.textContent.split(' ')[s.textContent.split(' ').length - 1], 10);
 
-          const bar = document.createElement('td');
-          bar.classList.add('course-stat-graph');
-          bar.innerHTML = `<div class="course-stat-bar" style="width: ${val}%"></div>`;
+      const heroimage = document.querySelector('div.hero picture');
+      const heroh1 = document.querySelector('div.hero h1');
 
-          const percent = document.createElement('td');
-          percent.classList.add('course-stat-percent');
-          percent.innerHTML = `${val}%`;
-
-          const thisStat = document.createElement('td');
-          thisStat.classList.add('course-stat-title');
-          thisStat.innerHTML = s.querySelector('strong').textContent;
-
-          tableRow.append(bar, percent, thisStat);
-          statsTable.append(tableRow);
-        }
-      });
-      if (allStats && statsTable) allStats.replaceWith(statsTable);
-
-      text.prepend(overview, statistics);
-      // setup photo credits
-      const credits = text.querySelector('p > em');
-      if (credits) credits.parentNode.classList.add('course-credits');
+      image.innerHTML = heroimage.outerHTML;
+      text.prepend(heroh1.cloneNode(true));
     }
     /* buttons */
     const button = document.createElement('button');
