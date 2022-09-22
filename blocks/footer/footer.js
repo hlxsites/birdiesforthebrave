2b4d2e18f09b59b480c2d57a475324be2f5a1071
Carsten Ziegeler <cziegeler@apache.org>
2022-09-22 18:10:22 +0200

    Fix js warnings

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -12,7 +12,7 @@ export default async function decorate(block) {
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
-  var template = document.createElement('template');
+  const template = document.createElement('template');
   template.innerHTML = html.trim();
   const footerHtml = template.content.firstChild;
   const footer = document.createElement('div');
@@ -21,7 +21,7 @@ export default async function decorate(block) {
     const n = footerHtml.firstChild;
     footerHtml.removeChild(n);
     const div = document.createElement('div');
-    div.append(n)
+    div.append(n);
     footer.append(div);
   };
   await decorateIcons(footer);
