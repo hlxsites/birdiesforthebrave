a346d094bd33719ec45c48a7bc09319ee398b438
Lars Trieloff <lars@trieloff.net>
2022-10-05 17:18:53 +0200

    style(delayed): auto-fix linting issues

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -9,67 +9,65 @@ sampleRUM('cwv');
 const isProd = window.location.hostname.endsWith('birdiesforthebrave.org');
 
 function loadScript(url, callback, type) {
-    const head = document.querySelector('head');
-    if (!head.querySelector(`script[src="${url}"]`)) {
-      const script = document.createElement('script');
-      script.src = url;
-      if (type) script.setAttribute('type', type);
-      head.append(script);
-      script.onload = callback;
-      return script;
-    }
-    return head.querySelector(`script[src="${url}"]`);
+  const head = document.querySelector('head');
+  if (!head.querySelector(`script[src="${url}"]`)) {
+    const script = document.createElement('script');
+    script.src = url;
+    if (type) script.setAttribute('type', type);
+    head.append(script);
+    script.onload = callback;
+    return script;
   }
-  
-  
-  const pageType = window.location.pathname === '/' ? 'homePage' : 'contentPage';
-  
-  const pname = window.location.pathname.split('/').pop();
+  return head.querySelector(`script[src="${url}"]`);
+}
 
-  window.OnetrustActiveGroups = ',C0002,';
-  window.OptanonActiveGroups = ',C0002,';
-  
-  window.pgatour = window.pgatour || {};
+const pageType = window.location.pathname === '/' ? 'homePage' : 'contentPage';
 
-  window.pgatour.tracking = {
-    branch: {
-      apiKey: 'key_live_nnTvCBCejtgfn40wtbQ6ckiprsemNktJ',
-      isWebView: 'false',
-    },
-    krux: {
-      id: '',
-    },
-    indexExchange: {
-      status: false,
-    },
-  };
-  
-  window.pgatour.Omniture = {
-    properties: {
-      pageName: `pgatour:birdiesforthebrave:${pname}`,
-      eVar16: `pgatour:birdiesforthebrave:${pname}`,
-      prop18: pageType,
-      eVar1: 'pgatour',
-      prop1: 'pgatour',
-      prop2: 'r011',
-      eVar2: 'r011',
-      eVar6: window.location.href,
-    },
-    defineOmnitureVars: () => {
-      if (window.s) {
-        Object.assign(window.s, window.pgatour.Omniture.properties);
-      }
-    },
-  
-  };
-  
-  window.pgatour.docWrite = document.write.bind(document);
-  
+const pname = window.location.pathname.split('/').pop();
+
+window.OnetrustActiveGroups = ',C0002,';
+window.OptanonActiveGroups = ',C0002,';
+
+window.pgatour = window.pgatour || {};
+
+window.pgatour.tracking = {
+  branch: {
+    apiKey: 'key_live_nnTvCBCejtgfn40wtbQ6ckiprsemNktJ',
+    isWebView: 'false',
+  },
+  krux: {
+    id: '',
+  },
+  indexExchange: {
+    status: false,
+  },
+};
+
+window.pgatour.Omniture = {
+  properties: {
+    pageName: `pgatour:birdiesforthebrave:${pname}`,
+    eVar16: `pgatour:birdiesforthebrave:${pname}`,
+    prop18: pageType,
+    eVar1: 'pgatour',
+    prop1: 'pgatour',
+    prop2: 'r011',
+    eVar2: 'r011',
+    eVar6: window.location.href,
+  },
+  defineOmnitureVars: () => {
+    if (window.s) {
+      Object.assign(window.s, window.pgatour.Omniture.properties);
+    }
+  },
+
+};
+
+window.pgatour.docWrite = document.write.bind(document);
 
 if (!localStorage.getItem('OptIn_PreviousPermissions')) {
-    const adobeSettings = {'aa': true, 'aam': true, 'ecid': true};
-    adobeSettings.tempImplied = true;
-    localStorage.setItem('OptIn_PreviousPermissions', JSON.stringify(adobeSettings));
+  const adobeSettings = { aa: true, aam: true, ecid: true };
+  adobeSettings.tempImplied = true;
+  localStorage.setItem('OptIn_PreviousPermissions', JSON.stringify(adobeSettings));
 }
 
 loadScript(`https://assets.adobedtm.com/d17bac9530d5/90b3c70cfef1/launch-1ca88359b76c${isProd ? '.min' : ''}.js`);
