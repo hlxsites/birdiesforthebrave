a14db1f3c097e714631eef81257170f190eb1cb9
Stephan Ringel <ringel@adobe.com>
2022-10-07 15:53:05 +0200

    fix linting

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,25 +14,25 @@ export default async function decorate(block) {
   const html = await resp.text();
   const template = document.createRange().createContextualFragment(html).firstElementChild;
   const footer = document.createDocumentFragment();
-  
+
   const logo = document.createElement('div');
   logo.append(template.firstElementChild);
-  
+
   const legal = document.createElement('div');
   legal.append(template.firstElementChild);
   while (template.firstElementChild.childNodes.length > 2) {
     template.firstElementChild.lastChild.remove();
   }
   legal.append(template.firstElementChild);
-  
+
   const social = document.createElement('div');
   social.append(template.firstElementChild);
-  
+
   footer.append(logo);
   footer.append(legal);
   footer.append(social);
-  
+
   block.append(footer);
-  
+
   decorateIcons(block);
 }
