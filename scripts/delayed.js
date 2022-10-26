4260a8ce5be11eaaccdfbf8a9d3efb1e25f2e9aa
David Nuescheler <david@nuescheler.ch>
2022-10-26 08:12:12 -0600

    chore: prod ot setup

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './scripts.js';
+import { sampleRUM, fetchPlaceholders } from './scripts.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
-
+const placeholders = await fetchPlaceholders();
 const isProd = window.location.hostname.endsWith('birdiesforthebrave.org');
 
 function loadScript(url, callback, type) {
@@ -21,12 +21,24 @@ function loadScript(url, callback, type) {
   return head.querySelector(`script[src="${url}"]`);
 }
 
+/* setup cookie preferences */
+function getCookie(cookieName) {
+  const name = `${cookieName}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const split = decodedCookie.split(';');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < split.length; i++) {
+    let c = split[i];
+    while (c.charAt(0) === ' ') c = c.substring(1);
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+  }
+  return null;
+}
+
+
 const pageType = window.location.pathname === '/' ? 'homePage' : 'contentPage';
 
 const pname = window.location.pathname.split('/').pop();
-
-window.OnetrustActiveGroups = ',C0002,';
-window.OptanonActiveGroups = ',C0002,';
 
 window.pgatour = window.pgatour || {};
 
@@ -59,15 +71,40 @@ window.pgatour.Omniture = {
       Object.assign(window.s, window.pgatour.Omniture.properties);
     }
   },
-
 };
 
 window.pgatour.docWrite = document.write.bind(document);
 
-if (!localStorage.getItem('OptIn_PreviousPermissions')) {
-  const adobeSettings = { aa: true, aam: true, ecid: true };
-  adobeSettings.tempImplied = true;
-  localStorage.setItem('OptIn_PreviousPermissions', JSON.stringify(adobeSettings));
+async function OptanonWrapper() {
+  const geoInfo = window.Optanon.getGeolocationData();
+  Object.keys(geoInfo).forEach((key) => {
+    const cookieName = `PGAT_${key.charAt(0).toUpperCase() + key.slice(1)}`;
+    const cookie = getCookie(cookieName);
+    if (!cookie || cookie !== geoInfo[key]) document.cookie = `${cookieName}=${geoInfo[key]}`;
+  });
+
+  const prevOptIn = localStorage.getItem('OptIn_PreviousPermissions');
+  if (prevOptIn) {
+    try {
+      const adobeSettings = JSON.parse(prevOptIn);
+      if (adobeSettings.tempImplied) {
+        localStorage.removeItem('OptIn_PreviousPermissions');
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error('OptIn_PreviousPermissions parse failed');
+    }
+  }
+
+  loadScript(`https://assets.adobedtm.com/d17bac9530d5/90b3c70cfef1/launch-1ca88359b76c${isProd ? '.min' : ''}.js`, () => {
+    dispatchEvent(new Event('load'));
+  });
 }
 
-loadScript(`https://assets.adobedtm.com/d17bac9530d5/90b3c70cfef1/launch-1ca88359b76c${isProd ? '.min' : ''}.js`);
+const otId = placeholders.onetrustId;
+if (otId) {
+  const cookieScript = loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js');
+  cookieScript.setAttribute('data-domain-script', `${otId}${isProd ? '' : '-test'}`);
+
+  window.OptanonWrapper = OptanonWrapper;
+}
